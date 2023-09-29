fb2c4fd1ca39224370ad4c107d252cd5c8c24694
Diego Mella <tinmell>
2023-09-29 20:59:22 -0300

    subiendo cambios

--- myapp/models.py
+++ myapp/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 class Project(models.Model):
     name = models.CharField(max_length=200)
-
+    def __str__(self):
+        return self.name
 class Task(models.Model):
     title=models.CharField(max_length=200)
     description=models.TextField()
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,9 @@
-#from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
 def hello(request):
-    return HttpResponse("<h2>Hola mundo</h2>")
+    title= 'Bienvenido'
+    return render(request, 'hello.html')
+#HttpResponse("<h2>Hola mundo</h2>")
 def about(request):
     return HttpResponse('About')
